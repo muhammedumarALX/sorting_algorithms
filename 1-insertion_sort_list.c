494fb4e75c3c6d7917280f602a2f389fae589a4f
muhammed umar <muhammedumaralx@gmail.com>
2023-01-25 02:50:07 +0100

    Task 2

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * insertion_sort_list - sorts a DLL of integers in
+ * ascending order using the insertion sort
+ * algoritm
+ *
+ * @list: doubly linked list
+ * Return: no return
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *ptr, *tmp;
+
+	if (!list)
+		return;
+
+	prt = *list;
+
+	while (ptr->next && (ptr->n > ptr->next->n))
+	{
+		tmp = ptr->next;
+		ptr->next = tmp->next;
+		tmp->prev = ptr->prev;
+
+		if (ptr->prev)
+			ptr->prev->next = tmp;
+
+		if (tmp->next)
+			tmp->next->prev = ptr;
+
+		ptr->prev = tmp;
+		tmp->next = ptr;
+
+		if (tmp->prev)
+			ptr = tmp->prev;
+		else
+			*list = tmp;
+
+		print_list(*list);
+	}
+}
