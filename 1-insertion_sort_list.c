add6355cf04eccf268a80f6ed07162f638b390f5
muhammed umar <muhammedumaralx@gmail.com>
2023-01-25 02:58:42 +0100

    Task 2

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,4 @@
-#include "sort.h"
+#include "sort.hi"
 #include <stdio.h>
 
 /**
@@ -18,26 +18,30 @@ void insertion_sort_list(listint_t **list)
 
 	prt = *list;
 
-	while (ptr->next && (ptr->n > ptr->next->n))
+	while (ptr)
 	{
-		tmp = ptr->next;
-		ptr->next = tmp->next;
-		tmp->prev = ptr->prev;
+		while (ptr->next && (ptr->n > ptr->next->n))
+		{
+			tmp = ptr->next;
+			ptr->next = tmp->next;
+			tmp->prev = ptr->prev;
 
-		if (ptr->prev)
-			ptr->prev->next = tmp;
+			if (ptr->prev)
+				ptr->prev->next = tmp;
 
-		if (tmp->next)
-			tmp->next->prev = ptr;
+			if (tmp->next)
+				tmp->next->prev = ptr;
 
-		ptr->prev = tmp;
-		tmp->next = ptr;
+			ptr->prev = tmp;
+			tmp->next = ptr;
 
-		if (tmp->prev)
-			ptr = tmp->prev;
-		else
-			*list = tmp;
+			if (tmp->prev)
+				ptr = tmp->prev;
+			else
+				*list = tmp;
 
-		print_list(*list);
+			print_list(*list);
+		}
+		ptr = ptr->next;
 	}
 }
